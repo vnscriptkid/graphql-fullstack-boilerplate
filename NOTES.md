5e7aad340646d8a9bbbf5e340348d47ddfff6a58
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-10 13:31:40 +0700

    Breakdown schema & resolvers

--- NOTES.md
+++ NOTES.md
@@ -7,3 +7,8 @@ typeorm init --name MyProject --database postgres
 ## Update `ormconfig.json`: db credentials
 ## Update `tsconfig.json`
 ## Config tslint
+## Graphql defs:
+- Schema: collection of `typeDefs`: Book, User (queryable fields)
+- resolvers: how to fetch data
+    - query: special type
+    - mutation: special type
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,10 @@
 import "reflect-metadata";
 
 import { GraphQLServer } from "graphql-yoga";
-// ... or using `require()`
-// const { GraphQLServer } = require('graphql-yoga')
+import { importSchema } from "graphql-import";
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || "World"}`,
-  },
-};
+import { resolvers } from "./resolvers";
+const typeDefs = importSchema("schema.graphql");
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 server.start(() => console.log("Server is running on localhost:4000"));
